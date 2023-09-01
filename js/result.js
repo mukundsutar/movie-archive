71457e4a741b1466d08b3d8b7b782defd8b563e1
Mukund Sutar <mukundsutar1512@gmail.com>
2023-09-01 23:56:46 +0530

    Testing CORS error

--- js/result.js
+++ js/result.js
@@ -1,21 +1,24 @@
 API_URL =
     "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=7c7034e65c22ade9db6191d62074a4e0&page=1";
 SEARCH_API =
-    "https://api.themoviedb.org/3/search/movie?api_key=7c7034e65c22ade9db6191d62074a4e0&query=";
+    "https://api.themoviedb.org/3/movie/346698?api_key=7c7034e65c22ade9db6191d62074a4e0";
 IMG_PATH = "https://image.tmdb.org/t/p/w1280";
 
+res.header("Access-Control-Allow-Origin", "*");
+
 function getID(id) {
-let poster= document.createElement("img");
+    console.log(id);
+    let poster = document.createElement("img");
     let posterDiv = document.getElementsByClassName("movie-poster");
 
     getDetails();
 }
 
 async function getDetails() {
-    let res = await fetch("https://api.themoviedb.org/3/movie/27205&api_key=7c7034e65c22ade9db6191d62074a4e0");
+    let res = await fetch(API_URL);
+    console.log(res);
     let data = await res.json();
-
-    console.log(data["results"]);
+    console.log(data);
 }
 
 function showPage() {
